e862b2b5f07bd0a493ca39d111d819bc055a6067
Lauren <lasell@gmail.com>
2019-11-23 10:18:26 -0800

    made changes!

---
+++ script.js
@@ -0,0 +1,42 @@
+'use strict'
+// $(function() {
+
+// $('.slideshow').square1({
+//     animation: 'slide',
+//     transition_time: 2000,
+//     prev_next_nav: 'hover',
+//     dots_nav: 'hover'
+// });
+
+
+function toggleIcon() {
+    $('.icon').on('click', function(){
+        $('.icon').toggleClass('active');
+        $('.menu-mobile').slideToggle(300);
+    })
+}
+
+function closeMobileMenu(){
+    $('.menu-mobile').on('click', 'a', function(){
+        $('.icon').trigger('click');
+    })
+}
+
+function desktopMenu() {
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 50 && $(window).width() > 768) {
+            $('header.desktop').fadeIn(500);
+        } else {
+            $('header.desktop').fadeOut(500);
+        }
+    })
+}
+
+
+
+
+
+//when the page loads call toggleIcon;
+$(toggleIcon);
+$(closeMobileMenu);
+$(desktopMenu);
