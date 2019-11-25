7d283e86283c6b2f240c9012d7c534129d88c9bc
Lauren <lasell@gmail.com>
2019-11-25 15:48:49 -0800

    made changes

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 'use strict'
+
 // $(function() {
 
-// $('.slideshow').square1({
-//     animation: 'slide',
-//     transition_time: 2000,
-//     prev_next_nav: 'hover',
-//     dots_nav: 'hover'
-// });
+$('.slideshow').square1({
+    animation: 'slide',
+    transition_time: 2000,
+    prev_next_nav: 'hover',
+    dots_nav: 'hover'
+});
+
+// new fullpage('#fullpage', {
+//           autoScrolling: true,
+//           navigation: true,
+//           navigationPosition: 'left',
+//           navigationTooltips: ['index', 'about', 'buy', 'blog',]
+//       })
 
 
 function toggleIcon() {
@@ -30,6 +38,14 @@ function desktopMenu() {
             $('header.desktop').fadeOut(500);
         }
     })
+}
+
+function myMap() {
+var mapProp= {
+  center:new google.maps.LatLng(51.508742,-0.120850),
+  zoom:5,
+};
+var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
 }
 
 
